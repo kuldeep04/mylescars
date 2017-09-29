ab294134d16fef76184c4c3728df72bd05dbfdb6
kuldeep Yadav <ykuldeep777@gmail.com>
2017-09-29 17:41:24 +0530

    restassuredapi

---
+++ mven-rest-assured-API/src/main/java/mven_rest_assured/mven_rest_assured_API/App.java
@@ -0,0 +1,13 @@
+package mven_rest_assured.mven_rest_assured_API;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
